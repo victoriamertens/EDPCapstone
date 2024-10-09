fed6821a315b425fa91f0f503a4c084f1929a163
Josh Susman <jsusman@travelers.com>
2024-10-09 16:46:39 -0400

    added prediction model and minor changes

--- populate_database.py
+++ populate_database.py
@@ -11,11 +11,21 @@ NUM_ROWS = 1000
 # Create the CSV file
 OUTPUT_FILE = "directory.csv"
 
-class roler:
-     def __init__(self, name, focus, level):
-          self.name = name
-          self.focus = focus
-          self.level = level
+class Role:
+    def __init__(self, name: str, focus: str):
+        self.name = name
+        self.focus = focus
+
+    def __str__(self):
+        return f"Role: {self.name}, Type: {self.focus}"
+
+class Location:
+    def __init__(self, name: str, population: int):
+        self.name = name
+        self.population = population
+
+    def __str__(self):
+        return f"Name: {self.name}, Population: {self.population}"
 
 def create_login(name):
     split = name.split(" ")
@@ -25,6 +35,92 @@ def create_login(name):
     password = "".join(random.choice(characters) for _ in range(lenght))
     return username, password
     
+def create_roles():
+    roles = [
+    "Software Engineer", "Data Scientist", "Product Manager", "UX/UI Designer", "Project Manager", 
+    "Business Analyst", "Systems Administrator", "Network Engineer", "Web Developer", 
+    "Content Strategist", "Social Media Manager", "Training and Development Manager", 
+    "Supply Chain Analyst", "Business Development Manager", "Event Planner", 
+    "Compliance Officer", "Insurance Underwriter", "Market Research Analyst", "Editorial Assistant", 
+    "Corporate Trainer", "E-commerce Specialist", "HR" 
+    ]
+    engineer = [0, 1, 8]
+    architecture = [6, 7]
+    management = [4, 10, 12, 14, 18, 20]
+    business = [2, 5, 9, 11, 15, 16, 19]
+    art = [3, 13, 17]
+    full_roles = []
+    for i in range(len(roles)):
+        if i in architecture:
+            role = Role(roles[i], "Architecture")
+            full_roles.append(role)
+        elif i in engineer:
+            role = Role(roles[i], "Engineer")
+            full_roles.append(role)
+        elif i in management:
+            role = Role(roles[i], "Management")
+            full_roles.append(role)
+        elif i in business:
+            role = Role(roles[i], "Business")
+            full_roles.append(role)
+        else:
+            role = Role(roles[i], "Art")
+            full_roles.append(role)
+    return full_roles
+
+def create_location():
+    cities = [
+    "New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", 
+    "San Diego", "Dallas",  "Jacksonville", "Fort Worth",
+    "San Francisco", "Charlotte", "Indianapolis", "Seattle", "Denver", "Washington D.C."
+    ]
+    populations = [8.34, 3.82, 2.67, 2.30, 1.64, 1.57, 1.47, 1.38, 1.3, 0.97, 
+                   0.95, 3.51, 0.89, 0.88, 0.75, 0.71, 3.58
+    ]
+    full_cities = []
+    for i in range(len(cities)):
+        city = Location(cities[i], populations[i])
+        full_cities.append(city)
+    return full_cities
+
+def calcuate_salary(focus, location):
+    value = 0
+    match focus:
+        case "Architecture":
+            value += random.randint(40, 50)
+        case "Engineer":
+            value += random.randint(30, 40)
+        case "Management":
+            value += random.randint(20, 30)
+        case "Business":
+            value += random.randint(10, 20)
+        case _:
+            value += random.randint(0, 10)
+    if location >= 3.50:
+        value += random.randint(40, 50)
+    elif (location < 3.50) and (location >= 2.0):
+        value += random.randint(30, 40)
+    elif (location < 2.0) and (location >= 1.25):
+        value += random.randint(20, 30)
+    elif (location < 1.25) and (location >= 0.90):
+        value += random.randint(10, 20)
+    else:
+        value += random.randint(0, 10)
+    if value >= 85:
+        salary = random.randint(165000, 200000)
+    elif (value < 85) and (value >= 70):
+        salary = random.randint(125000, 165000)
+    elif (value < 70) and (value >= 55):
+        salary = random.randint(85000, 125000)
+    elif (value < 55) and (value >= 40):
+        salary = random.randint(65000, 85000)
+    elif (value < 40) and (value >= 25):
+        salary = random.randint(40000, 65000)
+    else:
+        salary = random.randint(10000, 40000)
+    return salary
+
+
 
 fnames = [
     "John", "James", "Harry", "Leslie", "Amanda", "Emma", "Liam", "Olivia", "Noah", "Ava",
@@ -49,22 +145,6 @@ lnames = [
     "Perry", "Powell", "Long", "Patterson", "Hughes", "Flores", "Washington", "Butler", "Simmons", 
 ]
 
-roles = [
-    "Software Engineer", "Data Scientist", "Product Manager", "UX/UI Designer", "Project Manager", 
-    "Business Analyst", "Systems Administrator", "Network Engineer", "Web Developer", 
-    "Database Administrator", "DevOps Engineer", "Marketing Manager", "Sales Representative", 
-    "Customer Support Specialist", "Graphic Designer", "Content Writer", "Human Resources Manager", 
-    "Accountant", "Financial Analyst", "Operations Manager", "Research Scientist", 
-    "Quality Assurance Tester", "Digital Marketing Specialist", "SEO Analyst", 
-    "Content Strategist", "Social Media Manager", "Training and Development Manager", 
-    "Legal Advisor", "Technical Support Specialist", "Pharmaceutical Sales Representative", 
-    "Supply Chain Analyst", "Business Development Manager", "Event Planner", 
-    "Public Relations Specialist", "Recruitment Consultant", "Software Tester", "IT Consultant", 
-    "Systems Analyst", "Cloud Solutions Architect", "Mobile App Developer", "Hardware Engineer", 
-    "Compliance Officer", "Insurance Underwriter", "Market Research Analyst", "Editorial Assistant", 
-    "Corporate Trainer", "E-commerce Specialist", "Retail Manager", "Facilities Manager", "HR" 
-]
-
 cities = [
     "New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", 
     "San Diego", "Dallas", "San Jose", "Austin", "Jacksonville", "Fort Worth", "Columbus", 
@@ -74,15 +154,17 @@ cities = [
 # id, name, phone, role, location, salary, manager
 employees = []
 creds = []
+temp_names = []
 names = set()
-employees = []
 numbers = set()
+roles = create_roles()
+cities = create_location()
 for i in range(1, NUM_ROWS + 1):
     id = i
     while len(names) < i:
         name = random.choice(fnames) + " " + random.choice(lnames)
         names.add(name)
-    employees.append(name)
+    temp_names.append(name)
     username, password = create_login(name)
     while len(numbers) < i:
         first = random.randint(100, 999)
@@ -92,17 +174,17 @@ for i in range(1, NUM_ROWS + 1):
         numbers.add(phone)
     role = random.choice(roles)
     location = random.choice(cities)
-    salary = random.randint(10000, 140000)
+    salary = calcuate_salary(role.focus, location.population)
     if i != 1:
-        manager = random.choice(employees)
+        manager = random.choice(temp_names)
     else:
         manager = "none"
     data_row = [
         id,
         name,
         phone,
-        role,
-        location,
+        role.name,
+        location.name,
         salary,
         manager
     ]
