df2940e3e77832062a2583ccd55a65a6ffdc6444
vmertens <vmertens@travelers.com>
2024-10-09 15:00:42 -0400

    Added routes for pages

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,24 +3,36 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SearchPage from './components/SearchPage'
+import LoginPage from './components/LoginPage'
 import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Link
+  Link,
+  Navigate, 
+  redirect
 } from "react-router-dom";
 
 
 function App() {
-  const [count, setCount] = useState(0)
+const [isAuthenticated, setIsAuthenticated] = useState(false);
+// const navigate = useNavigate(); 
 
-
+// useEffect(() => {
+//   // Checking if user is not loggedIn
+//   if (!isAuthenticated) {
+//     navigate("/");
+//   } else {
+//     navigate("/login");
+//   }
+// }, [navigate, isLoggedIn]);
 
   return (
     <>
-     <h1>HomePage</h1>
     <Routes>
-      <Route path="/search" element={<SearchPage />}></Route>
+      <Route path="/" element={<h1>Home</h1>}></Route>
+      <Route path="/search" element={<SearchPage auth={isAuthenticated}/>}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
     </Routes>
     </>
   )
