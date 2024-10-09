b4314cc121eb088acbd7ea18a554dfedc570e337
vmertens <vmertens@travelers.com>
2024-10-09 15:01:39 -0400

    Added login endpoint to server

--- server/server.js
+++ server/server.js
@@ -5,20 +5,24 @@ import pg from 'pg'
 import { MongoClient, ObjectId } from 'mongodb';
 
 
+
 let app = express(); 
-app.use(express.json()); 
 dotenv.config();
 app.use(cors());
+app.use(express.json());
 
 //variables from env
 const url = process.env.MONGO_DB_URL;
 const dbName = process.env.MONGO_DB;
-const collectionName = process.env.MONGO_DB_COLLECTION;
+const collectionName = {search: process.env.MONGO_DB_COLLECTION_SEARCH, auth: process.env.MONGO_DB_COLLECTION_AUTH}
 
-async function connectToMongo () { 
+
+
+async function connectToMongo (collectionType) { 
     const client = await MongoClient.connect(url);
     const db = client.db(dbName);
-    const collection = db.collection(collectionName);
+    let selectedCollectionName = collectionName[collectionType]
+    const collection = db.collection(selectedCollectionName);
   return collection
 }
 
@@ -32,10 +36,26 @@ app.get('/', async (_ , res) => {
 app.get('/search/name/:name', async (req , res) => {
     let searchName = req.params.name;
         console.log("SEARCH NAME:", searchName);
-    let collection = await connectToMongo(); 
+    let collection = await connectToMongo("search"); 
     const response = await collection.find( { $text: { $search: `"${searchName}"` } } ).toArray();
     res.json(response);
 })
+
+app.post('/login', async (req , res) => {
+    let {username, password} = req.body; 
+    console.log("Logging in: ", username);
+    let collection = await connectToMongo("auth"); 
+   collection.findOne({"username": `${username}`, "password": `${password}`})
+    .then(
+        user => {
+            let response = {userId: user.userId}
+            res.json(response)}
+    ).catch(err => {
+        console.error('Error in querying database:', err)
+        res.sendStatus(500)
+    });
+}
+)
 
 
 
